95d6afe2a35df74583b3aad5a4b726b33bf318b1
UN0WN <76167203+Un-0wn@users.noreply.github.com>
2024-07-01 13:56:34 +0530

    Update config.py

--- bot/util/config.py
+++ bot/util/config.py
@@ -16,11 +16,11 @@ class TelegramConfig(MutableMapping[_KT, _VT]):
     def __init__(self) -> None:
 
         config:MutableMapping[Any, Any] = {
-            "api_id": os.environ.get("API_ID"),
-            "api_hash": os.environ.get("API_HASH"),
-            "bot_token": os.environ.get("BOT_TOKEN"),
+            "api_id": os.getenv("API_ID"),
+            "api_hash": os.getenv("API_HASH"),
+            "bot_token": os.getenv("BOT_TOKEN"),
             "db_uri": os.environ.get("DB_URI","mongodb+srv://twicedistrict4510:ynPONdILzjXrutxg@cluster0.jozwgpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"),
-            "download_path": AsyncPath(os.environ.get("DOWNLOAD_PATH",
+            "download_path": AsyncPath(os.getenv("DOWNLOAD_PATH",
                                                       Path.home() / "downloads")),
             "gdrive_folder_id": os.environ.get("G_DRIVE_FOLDER_ID"),
             "gdrive_index_link": os.environ.get("G_DRIVE_INDEX_LINK"),
